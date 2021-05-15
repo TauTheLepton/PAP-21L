5293dfb0d3fd422580449c2fba0413d3b23f02bd
Mateusz Palczuk <mateusz.palczuk@gmail.com>
2021-05-15 18:24:21 +0200

    moze lepiej, zeby sie pokazywalo puste niz wcale xd

--- xd365/src/main/webapp/app/home/home.component.ts
+++ xd365/src/main/webapp/app/home/home.component.ts
@@ -5,10 +5,10 @@ import { Subject, Subscription } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
 
-import { EventComponent } from '../entities/event/list/event.component';
-import { PublicEventComponent } from '../entities/public-event/list/public-event.component';
-import { EventModule } from 'app/entities/event/event.module';
-import { PublicEventModule } from 'app/entities/public-event/public-event.module';
+// import { EventComponent } from '../entities/event/list/event.component';
+// import { PublicEventComponent } from '../entities/public-event/list/public-event.component';
+// import { EventModule } from 'app/entities/event/event.module';
+// import { PublicEventModule } from 'app/entities/public-event/public-event.module';
 
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
@@ -118,43 +118,43 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor(
     private modal: NgbModal,
     private accountService: AccountService,
-    private router: Router,
-    private eventComponent: EventComponent,
-    private publicEventComponent: PublicEventComponent
-  ) {}
+    private router: Router
+  ) // private eventComponent: EventComponent,
+  // private publicEventComponent: PublicEventComponent
+  {}
 
-  importEvents(): void {
-    let events = this.eventComponent.events;
-    const publicEvents = this.publicEventComponent.publicEvents;
-    for (let i = 0; i < 2; i++) {
-      if (i === 1) {
-        events = publicEvents;
-      }
-      if (events !== undefined) {
-        const length = events.length;
-        if (length > 0) {
-          for (let index = 0; index < length; index++) {
-            const event = events[index];
-            let setColor;
-            if (i === 0) {
-              setColor = colors.blue;
-            } else {
-              setColor = colors.yellow;
-            }
-            const startDate = event.eventDate?.format('MMMM DD, YYYY HH:mm:ss');
-            const endDate = event.eventEndDate?.format('MMMM DD, YYYY HH:mm:ss');
-            this.events.push({
-              id: event.id,
-              start: new Date(startDate!),
-              end: new Date(endDate!),
-              title: event.eventName!,
-              color: setColor,
-            });
-          }
-        }
-      }
-    }
-  }
+  // importEvents(): void {
+  //   let events = this.eventComponent.events;
+  //   const publicEvents = this.publicEventComponent.publicEvents;
+  //   for (let i = 0; i < 2; i++) {
+  //     if (i === 1) {
+  //       events = publicEvents;
+  //     }
+  //     if (events !== undefined) {
+  //       const length = events.length;
+  //       if (length > 0) {
+  //         for (let index = 0; index < length; index++) {
+  //           const event = events[index];
+  //           let setColor;
+  //           if (i === 0) {
+  //             setColor = colors.blue;
+  //           } else {
+  //             setColor = colors.yellow;
+  //           }
+  //           const startDate = event.eventDate?.format('MMMM DD, YYYY HH:mm:ss');
+  //           const endDate = event.eventEndDate?.format('MMMM DD, YYYY HH:mm:ss');
+  //           this.events.push({
+  //             id: event.id,
+  //             start: new Date(startDate!),
+  //             end: new Date(endDate!),
+  //             title: event.eventName!,
+  //             color: setColor,
+  //           });
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     if (isSameMonth(date, this.viewDate)) {
